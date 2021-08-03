c2a6eafd339292d866b37c4b6cb802d192efd0d8
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2021-08-03 21:00:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-require('dotenv').config();
-const Discord = require('discord.js');
+require("dotenv").config();
+const Discord = require("discord.js");
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
-const prefix = "fdv ";
+const prefix = "leap ";
 
 bot.login(TOKEN);
 
-bot.on('ready', () => {
+bot.on("ready", () => {
   console.info(`Logged in as ${bot.user.tag}!`);
-  bot.user.setActivity('flairdive.com', { type: 'WATCHING' });
+  bot.user.setActivity("flairleap.com", { type: "WATCHING" });
 });
 
-bot.on('message', message => {
+bot.on("message", (message) => {
   if (message.content === `${prefix}ping`) {
-		message.channel.send(`Ping: ${bot.ws.ping}ms`);
-	}
+    message.channel.send(`Ping: ${bot.ws.ping}ms`);
+  }
 });
